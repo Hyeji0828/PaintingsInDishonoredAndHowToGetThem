c80b693d379ea5aef8930844960071708d0a6d91
Hyeji0828 <janghyeji0828@gmail.com>
2026-01-10 01:17:18 +0900

    상수를 모아둘 config 생성

---
+++ src/constants/config.js
@@ -0,0 +1,19 @@
+export const BASE_URL = "/CollectablesInDishonored/";
+
+export const TYPE_ORDER = ["paintings", "safes", "souvenir", "blueprints", "runes", "bonecharms"];
+export const NAME_MAP = {
+      "All" : "See All",
+      "m1" : "A Long Day in Dunwall",
+      "m2" : "Edge of the World",
+      "paintings" : "Painting",
+      "safes" : "Safe",
+      "souvenir" : "Souvenir", 
+      "blueprints" : "Blueprint", 
+      "runes" : "Rune", 
+      "bonecharms" : "Bonecharm"
+};
+
+export const GROUP_ORDER= {
+    mission: ["All", "m1", "m2"],
+    type: ["paintings", "safes", "souvenir", "blueprints", "runes", "bonecharms"]
+};
