c1a563fc9fa4efe96928daf1be032044141a89f2
Hyeji0828 <janghyeji0828@gmail.com>
2026-01-10 21:37:52 +0900

    화면 이동시 스크롤을 최상단으로 이동하는 ScrollToTop 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import {ByMission, ByType} from './pages/d2_collection';
 import { BASE_URL } from './constants/config';
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter basename={BASE_URL}>
+      <ScrollToTop/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/by-mission" element={<ByMission/>}/>
